f6ee2581b69bd638652c09cf7b995a7b78ffd8fa
Kwami11 <franckkwami11@gmail.com>
2023-02-16 17:43:46 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,7 +2,7 @@
 /**
  * main - A program that prints a line using puts function not printf.
  * Return: 0
- * /
+ */
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
