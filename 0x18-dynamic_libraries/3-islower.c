e291685ef1818800d55c7603fea999dcb32b4cc7
kwami11 <franckkwami11@gmail.com>
2023-06-19 15:22:52 -0700

    Dynamic libraries

---
+++ 0x18-dynamic_libraries/3-islower.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _islower - Checks for lowercase character
+ * @c: The character to be check
+ * Return: Returns 1 if c is lowercase, Returns 0 otherwise
+ */
+
+int _islower(int c)
+
+{
+	if (c >= 97 && c <= 122)
+	{
+
+	return (1);
+	}
+	return (0);
+}
